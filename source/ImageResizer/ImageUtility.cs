632dcf1471cca1855e7c5f6285ae24042adb25a9
Moein Shafiei <mrshafiei@hotmail.com>
2020-07-21 09:52:49 +0800

    initial commit

---
+++ source/ImageResizer/ImageUtility.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace ImageResizer
+{
+    public class ImageUtility
+    {
+        private static int i = 0;
+        public static void ReadResizeImages(string directory, Size size)
+        {
+            ResizeImages(directory, size);
+        }
+
+        private static List<string> ResizeImages(string directoryName, Size size)
+        {
+            List<string> filePaths = new List<string>();
+            if (Directory.Exists(directoryName))
+            {
+                var files = Directory.GetFiles(directoryName);
+                foreach (var file in files)
+                {
+                    ResizeImage(file, size);
+                }
+
+                var dirs = Directory.GetDirectories(directoryName);
+
+                foreach (var dir in dirs)
+                {
+                    ResizeImages(dir, size);
+                }
+            }
+
+            return filePaths;
+        }
+
+        private static void ResizeImage(string filePath, Size size)
+        {
+            Image image = Image.FromFile(filePath);
+            var backup = Path.GetDirectoryName(filePath) + "\\_" + Path.GetFileName(filePath);
+            image.Save(backup);
+
+
+            var resized = resizeImage(image, size);
+            image.Dispose();
+
+            File.Delete(filePath);
+            resized.Save(filePath);
+
+            File.Delete(backup);
+
+            Console.WriteLine((++i).ToString() + "   " + filePath);
+        }
+
+        private static Image resizeImage(Image imgToResize, Size size)
+        {
+
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+    }
+}
---
+++ source/ImageResizer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+
+namespace ImageResizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = ConfigurationManager.AppSettings.Get("directory");
+            int width = int.Parse(ConfigurationManager.AppSettings.Get("width_destin"));
+            int height = int.Parse(ConfigurationManager.AppSettings.Get("height_destin"));
+            ImageUtility.ReadResizeImages(dir, new Size(width, height));
+            Console.WriteLine("Press any key!");
+            Console.ReadLine();
+        }
+    }
+}
